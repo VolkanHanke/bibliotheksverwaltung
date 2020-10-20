b032e0d8628bdea24129355332a4669d806a0834
Volkan <volkanhanke@web.de>
2020-10-20 15:36:44 +0200

    sec commit

---
+++ src/main/java/de/vhanke/bibliotheksverwaltung/BibliotheksverwaltungApplication.java
@@ -0,0 +1,13 @@
+package de.vhanke.bibliotheksverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotheksverwaltungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotheksverwaltungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/vhanke/bibliotheksverwaltung/BibliotheksverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.vhanke.bibliotheksverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotheksverwaltungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
